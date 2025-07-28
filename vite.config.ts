362d06b7b267f40099e8d3588402fcc7d4b1c4a4
Mateus PS <mateus.castro2011@gmail.com>
2025-07-28 13:13:59 -0300

    feat: add custom CSS properties and configure path aliases in TypeScript and Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,13 @@ export default defineConfig({
   server: {
     port: 5123,
     strictPort: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/ui/assets'),
+      '@components': path.resolve(__dirname, './src/ui/components'),
+      '@pages': path.resolve(__dirname, './src/ui/pages'),
+    }
   }
 })
